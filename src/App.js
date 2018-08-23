c4552b142789a3fa82bfa7c669a026ade9e14dfa
Xiao, Peter <Peter.Xiao@cba.com.au>
2018-08-24 09:51:01 +1000

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import Quiz from "./Quiz";
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <div />;
+    return <Quiz />;
   }
 }
 
