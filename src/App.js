0bec29236a2b865f8b3c31d5266ab370b49ba272
Xiao, Peter <Peter.Xiao@cba.com.au>
2018-08-23 14:57:04 +1000

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
-    return (
-      <div />
-    )
+    return <div />;
   }
 }
 
-export default App
+export default App;
