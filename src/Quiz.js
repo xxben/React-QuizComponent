83d57e958e0d07303d8675f499719cb2382bf818
Xiao, Peter <Peter.Xiao@cba.com.au>
2018-08-24 16:01:50 +1000

    test

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import QuizQuestion from "./QuizQuestion";
 import QuizEnd from "./QuizEnd";
-import { stat } from "fs";
 
 let quizData = require("./quiz_data.json");
 
