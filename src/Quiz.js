81c36651903b8467f59c89d084c2061910b14eb6
Xiao, Peter <Peter.Xiao@cba.com.au>
2018-08-24 10:30:44 +1000

    test

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { render } from "enzyme";
+import QuizQuestion from "./QuizQuestion";
 
 let quizData = require("./quiz_data.json");
 
@@ -12,7 +12,9 @@ class Quiz extends Component {
   render() {
     return (
       <div>
-        <div>{quizData.quiz_questions[0].instruction_text}</div>
+        <div>
+          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+        </div>
       </div>
     );
   }
