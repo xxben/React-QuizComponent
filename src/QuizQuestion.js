3b9f6bed39f17cb3b0a7a75bbf76983efeda1bf6
Xiao, Peter <Peter.Xiao@cba.com.au>
2018-08-24 10:34:18 +1000

    test

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,14 +4,16 @@ class QuizQuestion extends Component {
   render() {
     return (
       <div>
-        <section>
-          <p>{this.props.quiz_question.instruction_text}</p>
-        </section>
-        <section className="button">
-          <ul>
-            <li>{this.props.quiz_question.answer_options[0]}</li>
-          </ul>
-        </section>
+        <main>
+          <section>
+            <p>{this.props.quiz_question.instruction_text}</p>
+          </section>
+          <section className="button">
+            <ul>
+              <li>{this.props.quiz_question.answer_options[0]}</li>
+            </ul>
+          </section>
+        </main>
       </div>
     );
   }
